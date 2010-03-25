13a310e9d87b5317d3b8a42bec83b91a61f8aa90
mjambon <mjambon@62bdb5e8-77b0-45a1-bc3e-26e3de145766>
2010-03-25 07:36:31 +0000

    Added missing module Bi_inbuf

---
+++ bi_inbuf.ml
@@ -0,0 +1,92 @@
+(* $Id$ *)
+
+type t = {
+  mutable i_s : string;
+  mutable i_pos : int;
+  mutable i_len : int;
+  mutable i_max_len : int;
+  i_refill : (t -> int -> unit)
+}
+
+exception End_of_input
+
+let try_preread ib n =
+  if ib.i_len - ib.i_pos < n then (
+    ib.i_refill ib n;
+    min (ib.i_len - ib.i_pos) n
+  )
+  else
+    n
+
+let read ib n =
+  let pos = ib.i_pos in
+  if ib.i_len - pos >= n then (
+    ib.i_pos <- pos + n;
+    pos
+  )
+  else
+    if try_preread ib n >= n then
+      let pos = ib.i_pos in
+      ib.i_pos <- ib.i_pos + n;
+      pos
+    else
+      raise End_of_input
+
+let read_char ib =
+  let pos = ib.i_pos in
+  if ib.i_len - pos > 0 then (
+    let c = String.unsafe_get ib.i_s pos in
+    ib.i_pos <- pos + 1;
+    c
+  )
+  else
+    if try_preread ib 1 > 0 then
+      let pos = ib.i_pos in
+      let c = String.unsafe_get ib.i_s pos in
+      ib.i_pos <- pos + 1;
+      c
+    else
+      raise End_of_input
+
+let from_string ?(pos = 0) s = {
+  i_s = s;
+  i_pos = pos;
+  i_len = String.length s;
+  i_max_len = String.length s;
+  i_refill = (fun ib n -> ())
+}
+
+(*
+  Like Pervasives.really_input but returns the number of bytes
+  read instead of raising End_of_file when the end of file is reached.
+*)
+let rec not_really_input ic s pos len accu =
+  let n = input ic s pos len in
+  if n < len then
+    if n = 0 then 0
+    else
+      not_really_input ic s (pos + n) (len - n) (accu + n)
+  else
+    accu + n
+
+let refill_from_channel ic ib n =
+  if n > ib.i_max_len then
+    invalid_arg "Bi_inbuf.refill_from_channel"
+  else (
+    let rem_len = ib.i_len - ib.i_pos in
+    if rem_len < n then
+      let s = ib.i_s in
+      String.blit s ib.i_pos s 0 rem_len;
+      let to_read = n - rem_len in
+      let really_read = not_really_input ic s rem_len to_read 0 in
+      ib.i_pos <- 0;
+      ib.i_len <- rem_len + really_read
+  )
+
+let from_channel ?(len = 4096) ic = {
+  i_s = String.create len;
+  i_pos = 0;
+  i_len = 0;
+  i_max_len = len;
+  i_refill = refill_from_channel ic
+}
---
+++ bi_inbuf.mli
@@ -0,0 +1,75 @@
+(* $Id$ *)
+
+type t = {
+  mutable i_s : string;
+    (* This is the buffer string.
+       It can be accessed for reading but should normally only
+       be written to or replaced only by the [i_refill] function.
+    *)
+
+  mutable i_pos : int;
+    (* This is the current position in the input buffer.
+       All data before that may be erased at anytime. *)
+
+  mutable i_len : int;
+    (*
+      This is the position of the first byte of invalid input data.
+      Data starting at [i_pos] and ending at [i_len-1] is considered
+      valid input data that is available to the user.
+      Beware that calls to [try_preread], [read] and other read functions
+      may move data around and therefore modify the values
+      of [i_pos] and [i_len] in order to keep pointing to the
+      correct data segment.
+    *)
+
+  mutable i_max_len : int;
+    (* This is the length of [i_s]. *)
+
+  i_refill : t -> int -> unit;
+    (*
+      Function called when not enough data is available in the buffer.
+      The int argument is the total number of bytes that must be 
+      available starting at position [i_pos] when the function returns.
+      This function typically does nothing if all input data already has been
+      placed into the buffer.
+      The [i_pos] and [i_len] fields can be modified the [i_refill] function,
+      as long as the available data that was starting from [i_pos]
+      still starts from the new value of [i_pos].
+      All the other fields can be modified as well.
+    *)
+}
+
+exception End_of_input
+
+val try_preread : t -> int -> int
+  (*
+    [try_preread ib n] make at least [n] bytes available for reading 
+    in [ib.i_s], unless the end of the input is reached.
+    The result indicates how many bytes were made available. If smaller than
+    [n], the result indicates that the end of the input was reached.
+    [ib.i_pos] is set to point to the first available byte.
+  *)
+
+val read : t -> int -> int
+  (*
+    [read ib n] makes at least [n] bytes available for reading or raises
+    the [End_of_input] exception.
+    The result is the position of the first available byte.
+    [ib.i_pos] is moved to point to the next position after the [n] bytes.
+  *)
+
+val read_char : t -> char
+  (*
+    Read just one byte.
+  *)
+
+val from_string : ?pos:int -> string -> t
+  (*
+    Create an input buffer from a string.
+  *)
+
+val from_channel : ?len:int -> in_channel -> t (* TODO: test this function *)
+  (*
+    Create an input buffer from an in_channel.
+    Such a buffer is not extensible and [read] requests may not exceed [len].
+  *)

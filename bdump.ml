539329e0c8f368ab08d3518765efc16606cb6d43
mjambon <mjambon@62bdb5e8-77b0-45a1-bc3e-26e3de145766>
2010-09-20 07:13:13 +0000

    Added global, auto-updated dictionary in ~/.bdump-dict or equivalent. New option -x disables this.

--- bdump.ml
+++ bdump.ml
@@ -19,9 +19,9 @@ let split s =
   String.sub s 0 !stop :: !acc
 
 
-let load_lines s =
+let load_lines accu s =
   let ic = open_in s in
-  let l = ref [] in
+  let l = ref accu in
   (try
      while true do
        l := input_line ic :: List.rev !l
@@ -42,18 +42,81 @@ let load ic =
   with End_of_file ->
     Buffer.contents buf
 
+let ( // ) = Filename.concat
+
+let global_dict_path =
+  try
+    match Sys.os_type with
+        "Unix" -> Some (Sys.getenv "HOME" // ".bdump-dict")
+      | "Win32" -> Some (Sys.getenv "HOMEPATH" // "_bdump-dict")
+      | "Cygwin" -> Some (Sys.getenv "HOME" // ".bdump-dict")
+      | _ -> None
+  with Not_found ->
+    None
+
+let load_global_dictionary accu =
+  match global_dict_path with
+      None -> accu
+    | Some fn ->
+        if Sys.file_exists fn then
+          try
+            load_lines accu fn
+          with e ->
+            failwith (sprintf "Cannot load global dictionary from %S: %s\n%!"
+                        fn (Printexc.to_string e))
+        else
+          accu
+
+let write_uniq oc a =
+  if Array.length a > 0 then (
+    fprintf oc "%s\n" a.(0);
+    ignore (
+      Array.fold_left (
+        fun last x ->
+          if last <> x then
+            fprintf oc "%s\n" x;
+          x
+      ) a.(0) a
+    )
+  )
+
+let save_global_dictionary l =
+  match global_dict_path with
+      None -> ()
+    | Some fn ->
+        let a = Array.of_list l in
+        Array.sort String.compare a;
+        let oc = open_out fn in
+        let finally () = close_out_noerr oc in
+        try
+          write_uniq oc a;
+          finally ()
+        with e ->
+          finally ();
+          raise e
+    
 let () =
   let dic = ref [] in
   let file = ref None in
+  let use_global_dictionary = ref true in
   let options = [
+    "-d", Arg.String (fun s -> dic := load_lines !dic s),
+    "file
+          File containing words to add to the dictionary, one per line";
+
     "-w", Arg.String (fun s -> dic := List.rev_append (split s) !dic),
     "word1,word2,...
           Comma-separated list of words to add to the dictionary";
 
-    "-d", Arg.String (fun s -> dic := List.rev_append (load_lines s) !dic),
-    "file
-          File containing words to add to the dictionary, one per line";
-  ] in
+    "-x", Arg.Clear use_global_dictionary,
+    sprintf "
+          Do not load nor update the global dictionary used for name
+          unhashing%s."
+      (match global_dict_path with
+           None -> ""
+         | Some s -> sprintf " (file %S)" s);
+  ]
+  in
   let msg = sprintf "Usage: %s [file] [options]" Sys.argv.(0) in
   let error () =
     Arg.usage options msg in
@@ -63,6 +126,13 @@ let () =
       | Some _ -> error ()
   in
   Arg.parse options set_file msg;
+
+  if !use_global_dictionary then (
+    let must_save = !dic <> [] in
+    dic := load_global_dictionary !dic;
+    if must_save then
+      save_global_dictionary !dic
+  );
 
   let unhash = Bi_io.make_unhash !dic in
   let ic = 
